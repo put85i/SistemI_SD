ff3efde02caf6873b5c8ae83cf6ef2c234a0653b
putri najwa <putrinajwass28@gmail.com>
2025-07-11 15:49:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -82,8 +82,8 @@ def contact():
 
     return "Pesan berhasil dikirim! Terima kasih telah menghubungi kami."
 
+import os 
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
